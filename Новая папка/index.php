0563f3f1e2cb2c5073df42aba0442ddab683e513
dem9967 <122559507+dem9967@users.noreply.github.com>
2023-07-28 16:08:44 +0300

    Add files via upload

---
+++ Новая папка/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>chat</title>
+    <link rel="stylesheet" href="style/style.css">
+    <script src="script/script.js"></script>
+    <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+</head>
+
+<body>
+    <form action="">
+        <div id="chat-result" class="chat-result"></div>
+    </form>
+</body>
+
+</html>
